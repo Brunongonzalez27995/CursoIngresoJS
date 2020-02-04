384fd6e835fc8180fa83827e8f5608a0e749c4bd
Brunongonzalez27995 <60636463+Brunongonzalez27995@users.noreply.github.com>
2020-02-04 19:07:52 -0300

    Add files via upload
    ejercicio alert resuelto

---
+++ jsEntradaSalida-1.js
@@ -0,0 +1,8 @@
+//Debemos lograr mostrar un mensaje al presionar el botón  'mostrar'.
+function mostrar()
+{
+	//var nombre = prompt ("hola","n n ");
+	//alert(nombre);
+	alert("Esto funciona de maravilla");
+}
+
