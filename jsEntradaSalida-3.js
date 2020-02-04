42cd2bc6ccbb40888612c72e3bf4264300ebaec3
Brunongonzalez27995 <60636463+Brunongonzalez27995@users.noreply.github.com>
2020-02-04 19:11:45 -0300

    Add files via upload
    ejercicio tomar datos por id y mostrar en alert

---
+++ jsEntradaSalida-3.js
@@ -0,0 +1,9 @@
+/*Debemos lograr tomar un dato por 'ID'
+y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
+function mostrar()
+{
+	nombre=document.getElementById('elNombre');
+	alert("Su nombre es "+nombre);
+}
+
+
