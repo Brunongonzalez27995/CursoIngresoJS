1474c38b587ff2adbfe7dce685d0e8aac033b976
Bruno <brunogonzalez27995@gmail.com>
2020-02-18 22:00:19 -0300

    Ejercicios resueltos

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -3,8 +3,13 @@ function mostrar()
 
 	var contador=0;
 	var acumulador=0;
-
-
+	contador=parseInt(contador);
+	switch(acumulador<5)
+	{
+		contador=contador+1;
+		contador=parseInt(contador);
+		acumulador=contador;
+	}
 
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/5;
